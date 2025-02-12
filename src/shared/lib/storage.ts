a755171eae52524fc9bc41e893f3b01e15c48226
Soprano7 <144703389+Soprano7@users.noreply.github.com>
2025-02-13 01:40:21 +0300

    Update storage.ts

--- src/shared/lib/storage.ts
+++ src/shared/lib/storage.ts
@@ -1,6 +1,6 @@
 const STORAGE_KEY = "itemsData";
 
-// Получаем данные из LocalStorage
+
 export const getStoredItem = (id: string) => {
     const data = localStorage.getItem(STORAGE_KEY);
     if (!data) return null;
@@ -8,7 +8,6 @@ export const getStoredItem = (id: string) => {
     return items[id] || null;
 };
 
-// Сохраняем изменения
 export const saveItem = (id: string, updatedData: { title: string; body: string }) => {
     const data = localStorage.getItem(STORAGE_KEY);
     const items = data ? JSON.parse(data) : {};
